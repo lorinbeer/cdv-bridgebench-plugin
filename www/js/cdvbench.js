594e720b19b50bb67b4e055389b3966267fdec0a
lorinbeer <lorin@adobe.com>
2013-08-15 18:04:33 -0700

    wrote new reader/writer implementation, trying to locate problem with original singleton

--- www/js/cdvbench.js
+++ www/js/cdvbench.js
@@ -1,29 +1,84 @@
 
-
+var cdvfs = null;
 
 var CDVBench = {
     run : function() {
         console.log("Cordova Bridge BenchTest initializing");
-        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, CDVBench.gotFS, CDVBench.fail);
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+        
     },
     
-    gotFS : function(cdvfs) {
-        console.log("Cordova Bridge BenchTest running");
-        cdvfs.root.getFile("Resources/random.task", null, CDVBench.gotFE, CDVBench.fail);
+    gotFS : function(fileSystem) {
+        fileSystem.root.getFile("random",
+                           {create:true, exclusive: false},
+                           function (fe) {
+                                console.log("got file, let's write");
+                                fe.createWriter( function(writer) {
+                                                console.log("writing some text");
+                                                    writer.write("somesampletextblahblahblah");
+                                                    cdvfs.root.getFile("random", null, CDVBench.gotFE, CDVBench.fail); },
+                                                    CDVBenchFail);
+                           },
+                           CDVBenchFail);
+    },
+    
+    initData : function (writer) {
+        console.log("writing some text");
+        writer.write("some sample text");
+        cdvfs.root.getFile("random", null, CDVBench.gotFE, CDVBench.fail);
     },
 
     gotFE : function(file) {
         console.log("got fe");
         var filereader = new FileReader();
-        filereader.onloadend = function (e) {
+        filereader.onloadend = function (evt) {
             console.log("read success");
-            console.log(new Uint8Array(evt.target.result));
+            console.log(evt.target);
         }
-        reader.readAsArrayBuffer(file);
+        filereader.readAsText(file);
     },
 
     fail : function(e) {
         console.log("FAIL");
-        console.log("Failed with Error Code", e.target.error.code);
+        console.log("Failed with Error Code", e.code);
     }
+      
+}
+
+function fail(e) {
+    console.log("FAILED", e);
+}
+
+function createWriter(fe) {
+    console.log(fe);
+    var fileEntry = fe;
+    fileEntry.createWriter(writeFile, fail);
+}
+
+function gotFileEntry(fe) {
+    console.log("READ FILE");
+    fe.file(gotFile, fail);
+    
+}
+
+function gotFile(f) {
+    var reader = new FileReader();
+    reader.onloadend = function(evt) {
+        console.log("Read as data URL");
+        console.log(new Uint8Array(evt.target.result));
+    };
+    reader.readAsArrayBuffer(f);
+}
+
+function writeFile(writer) {
+    console.log("WRITER FILE");
+    writer.onwriteend = function() {
+        cdvfs.root.getFile("dummy",null,gotFileEntry, fail);
+    };
+     writer.write("some sample text");
+}
+
+function writeFooFile(fileSystem) {
+    cdvfs = fileSystem;
+    cdvfs.root.getFile("dummy", {create: true,exclusive: false}, createWriter, fail);
 }
